c3bf8404c27b637586ff5405a3838175c7231db6
Minerofmillions <jasoncollins818@gmail.com>
2024-08-17 10:18:14 -0400

    Add corresponding twoDimensionArrayCollect to samples.

--- src/main/kotlin/io/github/minerofmillions/decompose/samples/CollectingListValueSamples.kt
+++ src/main/kotlin/io/github/minerofmillions/decompose/samples/CollectingListValueSamples.kt
@@ -6,5 +6,8 @@ import io.github.minerofmillions.decompose.collect
 /**
  * Collects a two-dimensional list of values to a value of a two-dimensional list.
  */
-fun <T : Any> twoDimensionListCollect(twoDimensionValueList: List<List<Value<T>>>): Value<List<List<T>>> =
+fun <T : Any> twoDimensionListCollect(twoDimensionValueList: List<List<Value<T>>>) =
     twoDimensionValueList.collect { it.collect() }
+
+fun <T : Any> twoDimensionArrayCollect(twoDimensionValueArray: Array<Array<Value<T>>>) =
+    twoDimensionValueArray.collect { it.collect() }
